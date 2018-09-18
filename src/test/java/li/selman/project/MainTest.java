09a0e0c1331f6461f91ae6d9907fdbee085cb60b
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-09-18 10:08:19 +0200

    Add JUnit5 support

---
+++ src/test/java/li/selman/project/MainTest.java
@@ -0,0 +1,16 @@
+package li.selman.project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Hasan Kara
+ */
+class MainTest {
+
+    @Test
+    void succeedingTest() {
+        assertEquals(1,1);
+    }
+}
