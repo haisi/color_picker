de275211f56880dd99444c314c632c44f136dbeb
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-09-18 10:26:12 +0200

    Add mockito support

--- src/test/java/li/selman/project/MainTest.java
+++ src/test/java/li/selman/project/MainTest.java
@@ -1,12 +1,15 @@
 package li.selman.project;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Hasan Kara
  */
+@ExtendWith(MockitoExtension.class)
 class MainTest {
 
     @Test
